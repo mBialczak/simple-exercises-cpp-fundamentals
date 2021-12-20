00eb26d638e74621d3cbc7a387fc8bb0f55a7d08
mBialczak <82645568+mBialczak@users.noreply.github.com>
2021-12-20 18:57:06 +0100

    TASK: modifyString
    modify_string function completed

---
+++ modifyString/main.cpp
@@ -0,0 +1,18 @@
+/* simple task of defining function modifing string */
+#include <iostream>
+#include <string>
+
+// TODO: Implement modifyString()
+// It should modify passed string to text "Other string"
+void modifyString(std::string &str)
+{
+  str = "Other string";
+}
+
+int main()
+{
+  std::string str{"Some string"};
+  modifyString(str);
+  std::cout << str << '\n';
+  return 0;
+}
